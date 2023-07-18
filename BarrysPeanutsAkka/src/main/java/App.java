d226d6d874424d6526190f5f320a24c416e82a47
Bob Reselman <bob@CogArtTech.com>
2023-07-17 19:42:23 -0700

    Converted shopping cart

--- BarrysPeanutsAkka/src/main/java/App.java
+++ BarrysPeanutsAkka/src/main/java/App.java
@@ -1,44 +1,44 @@
+import actor.CustomerActor;
 import actor.ShoppingCartActor;
-import akka.actor.typed.ActorSystem;
+import akka.actor.Props;
 import akka.actor.typed.ActorRef;
-import msg.*;
+import akka.actor.typed.ActorSystem;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
+import msg.Address;
+import msg.CreditCard;
+import msg.Customer;
+import msg.PurchaseItem;
+import scala.concurrent.Await;
+
+import akka.util.Timeout;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+
+import java.math.BigDecimal;
+
+
+import java.util.concurrent.TimeoutException;
+
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, TimeoutException {
         System.out.println("Starting Barry's Gourmet Peanuts");
-        /*
-        Address address = new Address("123 Main Street",
-                "Apt 1",
-                "Anytown",
-                "CA",
-                "99999-9999",
-                "USA");
-
-        Customer customer = new Customer("Barney",
-                "Rubble",
-                "barney@rubble.com",
-                "310 878 9999", address);
-
-        CreditCard creditCard = new CreditCard(customer.getFirstName() + " " + customer.getLastName(),
-              "1111222233334444",8, 26,111 );
-*/
         Customer customer = helper.MockHelper.getCustomer();
         Address address = helper.MockHelper.getAddress();
-        CreditCard creditCard = helper.MockHelper.getCreditCard(customer.getFirstName(), customer.getLastName());
 
         PurchaseItem purchase = new PurchaseItem(customer, "Barry's Gourmet Peanuts",5, 1,10.99, address, address);
-        // customerActor.tell(purchase);
-        ActorRef<ShoppingCartActor.Command> shoppingCartActor = ActorSystem.create(ShoppingCartActor.behavior(), "shoppingCartActor");
-        ShoppingCartActor.Command command  = new ShoppingCartActor.Command(purchase,ShoppingCartActor.Command.Action.ADD);
-        shoppingCartActor.tell(command);
-        shoppingCartActor.tell(command);
-        shoppingCartActor.tell(command);
-
-        command  = new ShoppingCartActor.Command(purchase,ShoppingCartActor.Command.Action.CHECKOUT);
-        shoppingCartActor.tell(command);
-
-
-    }
+        ShoppingCartActor.AddItem item = new ShoppingCartActor.AddItem(purchase);
+        ActorSystem<Object> shoppingCartActor = ActorSystem.create(ShoppingCartActor.create(), "shoppingCartActor");
+        shoppingCartActor.tell(item);
+        shoppingCartActor.tell(item);
+        shoppingCartActor.tell(item);
+        ShoppingCartActor.CheckoutCart checkout = new ShoppingCartActor.CheckoutCart();
+        shoppingCartActor.tell(checkout);
+        //TODO figure out away to call back to the Shopping Cart when the actual Checkout completes
+        ShoppingCartActor.EmptyCart emptyCart = new ShoppingCartActor.EmptyCart();
+        shoppingCartActor.tell(emptyCart);
+;    }
 }
 
 
--- BarrysPeanutsAkka/src/main/java/actor/ShoppingCartActor.java
+++ BarrysPeanutsAkka/src/main/java/actor/ShoppingCartActor.java
@@ -3,81 +3,117 @@ package actor;
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
-import akka.actor.typed.javadsl.*;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+import msg.*;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Vector;
 
-import msg.PurchaseItem;
-import msg.CheckOutItems;
+import akka.util.Timeout;
 
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
-public class ShoppingCartActor extends AbstractBehavior<ShoppingCartActor.Command> {
+public class ShoppingCartActor extends AbstractBehavior<Object> {
 
-    private final Vector<PurchaseItem> items;
-
-    public Vector<PurchaseItem> getItems() {
-        return items;
+    private ShoppingCartActor(ActorContext<Object> context) {
+        super(context);
+        this.purchaseItems = new Vector<PurchaseItem>();
     }
 
-    public ShoppingCartActor(ActorContext<ShoppingCartActor.Command> context) {
-        super(context);
-        this.items = new Vector<PurchaseItem>();
+    public static Behavior<Object> create() {
+        return Behaviors.setup(ShoppingCartActor::new);
+    }
+
+    public static Behavior<Object> behavior() {
+        return Behaviors.setup(ShoppingCartActor::new);
     }
 
     @Override
-    public Receive<Command> createReceive() {
+    public Receive<Object> createReceive() {
         return newReceiveBuilder()
-                .onMessage(Command.class, this::handleCommand)
+                .onMessage(AddItem.class, this::handleAddItem)
+                .onMessage(RemoveItem.class, this::handleRemoveItem)
+                .onMessage(EmptyCart.class, this::handleEmptyCart)
+                .onMessage(CheckoutCart.class, this::handleCheckoutCart)
                 .build();
     }
 
-    private String getNowString(Date date){
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return dateFormat.format(date);
-    }
-    private Behavior<Command> handleCommand(Command command){
-        Date today = new Date();
 
-        if(Command.Action.ADD == command.getAction() ){
-            this.items.add(command.getPurchaseItem());
-        } else if (Command.Action.CHECKOUT == command.getAction() ) {
-            ActorRef<Object> checkoutActor = ActorSystem.create(CheckOutActor.create(), "checkoutActor");
-            CheckOutActor.StartCheckout startCheckout = new CheckOutActor.StartCheckout(this.getItems());
-            checkoutActor.tell(startCheckout);
-        }else if(Command.Action.REMOVE == command.getAction() ) {
-            this.items.remove(command.getPurchaseItem());
-        }
+    private Behavior<Object> handleAddItem(AddItem msg) {
+        this.purchaseItems.add(msg.purchaseItem);
         return this;
     }
 
-    public static Behavior<Command> behavior(){
-        return (Behavior<ShoppingCartActor.Command>) Behaviors.setup(ShoppingCartActor::new);
+    private Behavior<Object> handleRemoveItem(RemoveItem msg) {
+        this.purchaseItems.remove(msg.purchaseItem);
+        return this;
     }
 
-    public static class Command {
-        public enum Action {
-            ADD,
-            REMOVE,
-            CHECKOUT
+
+    private Behavior<Object> handleEmptyCart(EmptyCart msg) {
+        String str = String.format("ShoppingCart is emptying the cart of %s items a checkout at %s. \n ", this.purchaseItems.toArray().length, new Date());
+        System.out.println(str);
+        this.purchaseItems = new Vector<PurchaseItem>();
+        return this;
+    }
+
+    private Behavior<Object> handleCheckoutCart(CheckoutCart msg) {
+        String str = String.format("ShoppingCart is starting a checkout of %s items a checkout at %s. \n", this.purchaseItems.toArray().length, new Date());
+        System.out.println(str);
+        ActorRef<Object> checkoutActor = ActorSystem.create(CheckOutActor.create(), "checkoutActor");
+        CheckOutActor.StartCheckout startCheckout = new CheckOutActor.StartCheckout(this.purchaseItems);
+        checkoutActor.tell(startCheckout);
+        return this;
+    }
+
+    Vector<PurchaseItem> purchaseItems;
+    public static class AddItem {
+        public AddItem(PurchaseItem purchaseItem) {
+            this.purchaseItem = purchaseItem;
         }
 
         public PurchaseItem getPurchaseItem() {
             return purchaseItem;
         }
 
-        public Command(PurchaseItem purchaseItem, Action action) {
-            this.purchaseItem = purchaseItem;
-            this.action = action;
-        }
-
-        public PurchaseItem purchaseItem;
-
-        Action action;
-
-        public Action getAction() {
-            return action;
-        }
+        PurchaseItem purchaseItem;
     }
+
+    public static class RemoveItem {
+        public RemoveItem(PurchaseItem purchaseItem) {
+            this.purchaseItem = purchaseItem;
+        }
+
+        public PurchaseItem getPurchaseItem() {
+            return purchaseItem;
+        }
+
+        PurchaseItem purchaseItem;
+    }
+
+    public static class EmptyCart {
+        public EmptyCart() {
+            this.emptyCartDate = new Date();
+        }
+        public Date getEmptyCartDate() {
+            return emptyCartDate;
+        }
+        Date emptyCartDate;
+    }
+
+    public static class CheckoutCart {
+        public CheckoutCart() {
+            this.checkoutCartDate = new Date();
+        }
+        public Date getEmptyCartDate() {
+            return checkoutCartDate;
+        }
+        Date checkoutCartDate;
+    }
+    
 }
